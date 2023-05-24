ddcaf3a37ee2c3541f398f00bd57e480c74065f5
marufhasan566 <marufhasan.iitju47@gmail.com>
2023-05-24 21:20:17 +0600

    C_Simple_Calculator

--- C_Simple_Calculator.cpp
+++ C_Simple_Calculator.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-    int a, b;
+    long long int a, b;
     cin >> a >> b;
+    // long long int multiplication = a * b;
     cout << a << " + " << b << " = " << a + b << endl;
     cout << a << " * " << b << " = " << a * b << endl;
+    // cout << a << " * " << b << " = " << multiplication << endl;
     cout << a << " - " << b << " = " << a - b << endl;
     return 0;
 }
